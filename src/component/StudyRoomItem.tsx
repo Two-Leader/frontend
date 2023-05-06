48bfb48da421842c142fd4c073d4120234e05f1f
Kyongmin1016 <rkdrnwkd1@gmail.com>
2023-05-06 17:43:10 +0900

    Feat:CreateUser API 연동

--- src/component/StudyRoomItem.tsx
+++ src/component/StudyRoomItem.tsx
@@ -5,6 +5,7 @@ import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 interface StudyRoomProps {
   roomUuid: string;
@@ -16,11 +17,27 @@ export default function StudyRoomItem({ roomUuid, roomName }: StudyRoomProps) {
     useState<boolean>(false);
   const handleCreateUserModalClose = () => setShowCreateUserModal(false);
   const handleCreateUserModalShow = () => setShowCreateUserModal(true);
+  const [userNameValue, setUserNameValue] = useState<string>('');
 
   const navigate = useNavigate();
-
-  const goStudyRoom = (e: any) => {
-    return navigate(`studyRoom/${roomUuid}`);
+  const goStudyRoom = () => {
+    axios
+      .post('http://localhost:8080/api/v1/users', {
+        userName: userNameValue,
+        roomUuid: `${roomUuid}`,
+      })
+      .then((response) => {
+        if (response.status === 201) {
+          sessionStorage.setItem(
+            `${roomUuid}`,
+            `${response.data.data.userUuid}`,
+          );
+          navigate(`studyRoom/${roomUuid}`);
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+      });
   };
 
   return (
@@ -40,7 +57,12 @@ export default function StudyRoomItem({ roomUuid, roomName }: StudyRoomProps) {
           <Form>
             <Form.Group>
               <Form.Label>이름</Form.Label>
-              <Form.Control type="text" autoFocus id="userName" />
+              <Form.Control
+                type="text"
+                autoFocus
+                id="userName"
+                onChange={(e) => setUserNameValue(e.target.value)}
+              />
             </Form.Group>
           </Form>
         </Modal.Body>
