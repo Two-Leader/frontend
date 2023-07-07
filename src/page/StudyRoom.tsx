e540eb3359659bc37905eb8a3099cae6ecc3e862
jung-yeon99 <jungyeon9130@naver.com>
2023-07-08 01:03:41 +0900

    feat: add StudyRoom & Footer
    feat: add StudyRoom & Footer

--- src/page/StudyRoom.tsx
+++ src/page/StudyRoom.tsx
@@ -2,9 +2,22 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import axios from 'axios';
 import { BASE_URL } from 'hooks/BaseUrl';
+import Webcam from 'react-webcam';
 import logo from '../assets/logo.svg';
-import { Nav, NavLink, NavBtn, NavBtnLink, FormBtnLink } from './css/Navbar';
-import { User, Room } from './css/Footer';
+import { Nav, NavLink } from './css/Navbar';
+import {
+  User,
+  Room,
+  Footer,
+  FooterBtn,
+  FooterBtnLink,
+  ScreenBtnLink,
+  WebcamContainer,
+  ExitBtnLink,
+} from './css/Footer';
+import mic from '../assets/mic.svg';
+import video from '../assets/video.svg';
+import screen from '../assets/screen.svg';
 
 export default function StudyRoom() {
   return (
@@ -14,8 +27,35 @@ export default function StudyRoom() {
           <img src={logo} alt="logo" />
         </NavLink>
       </Nav>
-      <User />
-      <Room />
+      {/* User와 Room을 감싸는 컨테이너 */}
+      <div style={{ display: 'flex' }}>
+        <div style={{ flexDirection: 'column' }}>
+          <User />
+          <Room />
+        </div>
+        <WebcamContainer>
+          {/* Webcam을 감싸는 컨테이너 */}
+          <Webcam style={{ width: '450px', height: '350px' }} />
+          <Webcam style={{ width: '450px', height: '350px' }} />
+          <Webcam style={{ width: '450px', height: '350px' }} />
+          <Webcam style={{ width: '450px', height: '350px' }} />
+        </WebcamContainer>
+      </div>
+
+      <Footer>
+        <FooterBtn>
+          <FooterBtnLink to="/">
+            <img src={video} alt="video" />
+          </FooterBtnLink>
+          <FooterBtnLink to="/">
+            <img src={mic} alt="mic" />
+          </FooterBtnLink>
+          <ScreenBtnLink to="/">
+            <img src={screen} alt="screen" />
+          </ScreenBtnLink>
+          <ExitBtnLink to="/">EXIT</ExitBtnLink>
+        </FooterBtn>
+      </Footer>
     </div>
   );
 }
--- src/page/css/Footer.ts
+++ src/page/css/Footer.ts
@@ -26,3 +26,96 @@ export const Room = styled.div`
   width: 20vw;
   box-shadow: 2px 2px 3px 1px rgba(0, 0, 0, 0.2);
 `;
+
+export const Footer = styled.footer`
+  background: #181d23;
+  height: 6vh;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  padding: 0.5rem calc((100vw - 1300px) / 2);
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+`;
+
+export const FooterBtn = styled.footer`
+  display: flex;
+  align-items: center;
+  margin-right: 24px;
+  /* Third Nav */
+  /* justify-content: flex-end;
+  width: 100vw; */
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+export const FooterBtnLink = styled(Link)`
+  border-radius: 20px;
+  background: #3b71ca;
+  padding: 5px 20px;
+  color: #fff;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+  font-size: 16px;
+  font-family: 'Pretendard-Medium';
+
+  /* Second Nav */
+  margin-left: 10px;
+
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background: #204c95;
+    color: #fff;
+  }
+`;
+export const ScreenBtnLink = styled(Link)`
+  border-radius: 20px;
+  background: #3b71ca;
+  padding: 5px 10px;
+  color: #fff;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+  font-size: 16px;
+  font-family: 'Pretendard-Medium';
+
+  /* Second Nav */
+  margin-left: 10px;
+
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background: #204c95;
+    color: #fff;
+  }
+`;
+export const ExitBtnLink = styled(Link)`
+  border-radius: 20px;
+  background: #d10202;
+  padding: 5px 20px;
+  color: #fff;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+  font-size: 16px;
+  font-family: 'Pretendard-Medium';
+  text-align: center;
+
+  /* Second Nav */
+  margin-left: 10px;
+
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background: #ac0000;
+    color: #fff;
+  }
+`;
+export const WebcamContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 50%);
+  grid-template-rows: repeat(2, 1fr);
+  gap: 10px;
+  margin-left: 8%;
+  margin-top: 13px;
+`;
